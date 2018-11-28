1ab073df8f8d1c69e6e2e0e17ace2a616bc86145
Oluwafemi Adeniji <femicodes@gmail.com>
2018-11-28 09:47:16 +0100

    scraped my portfolio

--- scrape.js
+++ scrape.js
@@ -1,8 +1,16 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://femicodes.github.io/', (error, res, html) => {
+request('https://femicodes.github.io', (error, res, html) => {
     if (!error && res.statusCode == 200) {
         const $ = cheerio.load(html);
+        const site = $('.description');
+        
+        // console.log(site.text());
+        
+        // const output = site.children().text();
+
+        const output = site.parent().text();
+        console.log(output);
     }
 });
